e08cf192556f53683f35d92989131504d892a0e4
zyltzero <46280578+zyltzero@users.noreply.github.com>
2020-10-25 15:37:20 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 #Disable root password
-sed -i '34s/^/# /' package/lean/default-settings/files/zzz-default-settings
+sed -i '33s/^/# /' package/lean/default-settings/files/zzz-default-settings
 
 #lcui-add
 #git clone https://github.com/rosywrt/luci-theme-rosy.git package/rosy
