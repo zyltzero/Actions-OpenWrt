b2994acb9503123cda59865b40386d0a0d515c18
zyltzero <46280578+zyltzero@users.noreply.github.com>
2021-05-03 11:00:33 +0800

    lcui-add

--- diy.sh
+++ diy.sh
@@ -6,7 +6,7 @@
 # Blog: https://p3terx.com
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 #Disable root password
 sed -i '33s/^/# /' package/lean/default-settings/files/zzz-default-settings
 
@@ -15,9 +15,10 @@ echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.defa
 echo "src-git lienol https://github.com/Lienol/openwrt-package" >> feeds.conf.default
 
 #lcui-add
-#git clone https://github.com/rosywrt/luci-theme-rosy.git package/rosy
+git clone https://github.com/rosywrt/luci-theme-rosy.git package/rosy
 git clone https://github.com/zyltzero/OpenClash.git package/OpenClash
 git clone https://github.com/frainzy1477/luci-app-clash.git package/clash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-#git clone https://github.com/zyltzero/Lienol-obsolete.git package/passwall
+git clone https://github.com/zyltzero/Lienol-obsolete.git package/passwall
+git clone https://github.com/tty228/luci-app-serverchan.git package/serverchan
 ./scripts/feeds update -a && ./scripts/feeds install -a
